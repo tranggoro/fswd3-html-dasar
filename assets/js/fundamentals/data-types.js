959d28b434971827b894d414c6211abf603e07dd
DESKTOP-L8SKOC7\trian <anggoro>
2023-03-15 14:14:31 +0700

    [FEAT] add java script

---
+++ assets/js/fundamentals/data-types.js
@@ -0,0 +1,25 @@
+
+const bigInt = 1234567890123456789012345678901234567890n;
+
+
+const nama1 = "Arkatama";
+const nama2 = "Arkatama";
+const nama3 = `Arkatama`;
+
+let isLoggedIn = true;
+let isAdmin = false;
+
+let isGreater = 10 > 5;
+let isLess = 10 < 5;
+
+
+let girlFriend = null;
+
+
+let name;
+console.log("name");
+
+let employee = Symbol("Joko");
+console.log(employee);
+console.log(employee.description);
+
